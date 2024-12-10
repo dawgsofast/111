536c8c90ae81a540b075ad84550f694704bf8fb0
dawgsofast <meltriss5445@gmail.com>
2024-12-10 21:09:20 +0100

    player movement
    ge1r2g214g3

---
+++ 123/Assets/movement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    private int gregr = 21;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
